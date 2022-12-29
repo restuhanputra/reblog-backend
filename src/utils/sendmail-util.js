0bad5da4a841530bfb2db1f92aa6546974ffcfa8
restuhanputra <restu.hanputraa@gmail.com>
2022-12-29 10:31:37 +0700

    feat: utils

---
+++ src/utils/sendmail-util.js
@@ -0,0 +1,44 @@
+import nodemailer from 'nodemailer';
+import config from '../config';
+
+const sendMail = async (to, subject, text, html) => {
+  try {
+    let mailTransporter;
+    /* Using Gmail for production */
+    if (config.NODE_ENV === 'production') {
+      mailTransporter = await nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: GMAIL_EMAIL,
+          pass: GMAIL_PASSWORD,
+        },
+      });
+    } else {
+      /* Using Mailtrap for development */
+      mailTransporter = await nodemailer.createTransport({
+        host: config.MAILTRAP.HOST,
+        port: config.MAILTRAP.PORT,
+        auth: {
+          user: config.MAILTRAP.USER,
+          pass: config.MAILTRAP.PASSWORD,
+        },
+      });
+    }
+
+    const mailDetails = {
+      from: config.GMAIL_EMAIL,
+      to: to,
+      subject: subject,
+      text: text,
+      html: html,
+    };
+
+    // const result = await mailTransporter.sendMail(mailDetails);
+    return await mailTransporter.sendMail(mailDetails);
+    // console.log('Email sent successfully: ', result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default sendMail;
