caedb0a52fab2f50e111237d1554f5653690f1b1
restuhanputra <restu.hanputraa@gmail.com>
2022-12-29 10:32:54 +0700

    feat(validator): user

---
+++ src/validators/user-validator.js
@@ -0,0 +1,17 @@
+import { check } from 'express-validator';
+
+const name = check('name', 'Name is required').not().isEmpty();
+const username = check('username', 'Username is required').not().isEmpty();
+const email = check('email')
+  .not()
+  .isEmpty()
+  .withMessage('Email is required')
+  .bail()
+  .isEmail()
+  .withMessage('Please provide a valid email');
+const password = check('password', 'Password is required').isLength({ min: 6 });
+
+export const RegisterValidation = [name, username, email, password];
+export const UpdateValidation = [name, username, email, password];
+// export const AuthenticatedValidation = [username, password];
+// export const ResetPasswordValidation = [email];
