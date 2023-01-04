3eb6b1fee3ef922383ee30c8ab2cf29842d8be3c
restuhanputra <restu.hanputraa@gmail.com>
2023-01-04 11:49:55 +0700

    fix(post-validator): add validate categoryId (#497c0f1)

--- src/validators/post-validator.js
+++ src/validators/post-validator.js
@@ -4,9 +4,16 @@ const title = check('title', 'Title is required').not().isEmpty();
 const content = check('content', 'Content is required').not().isEmpty();
 const image = check('image').custom((value, { req }) => {
   if (!req.files) throw new Error('Image is required');
+  return true;
+});
+const categoryId = check('categoryId').custom((value) => {
+  // check length array
+  if (!value.length) throw new Error('CategoryId is required');
 
+  // check type array
+  if (!Array.isArray(value)) throw new Error('CategoryId must be an array');
   return true;
 });
 
-export const PostValidation = [title, content, image];
-export const UpdatePostValidation = [title, content, image];
+export const PostValidation = [title, content, image, categoryId];
+export const UpdatePostValidation = [title, content, image, categoryId];
